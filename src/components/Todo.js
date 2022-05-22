9d23ee134710097fb87413aa885dafd9b6dad10b
tatsuro1997 <minagawa@dptr.jp>
2022-05-23 08:48:22 +0900

    Add useState in Todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,14 +1,24 @@
+import { useState } from "react";
+import Backdrop from "./Backdrop";
+import Modal from "./Modal";
+
 function Todo(props) {
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+
     function deleteHadler() {
-        
+        setModalIsOpen(true);
     }
 
     return (
       <div className="card">
         <h2>{props.text}</h2>
         <div className="actions">
-          <button className="btn" onClick={deleteHadler}>Delete</button>
+          <button className="btn" onClick={deleteHadler}>
+            Delete
+          </button>
         </div>
+        {modalIsOpen && <Modal />}
+        {modalIsOpen && <Backdrop />}
       </div>
     );
 }
