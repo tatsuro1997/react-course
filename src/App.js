158402b14298382ef7f35458732938d6ee3829a5
tatsuro1997 <minagawa@dptr.jp>
2022-05-23 17:47:07 +0900

    Adding Routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import FavoritesPage from "./pages/FavoritesMeetup";
+import NewMeetupPage from "./pages/NewMeetup";
+
 function App() {
   return (
     <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AllMeetupsPage />} />
+          <Route path="/new-meetup" element={<NewMeetupPage />} />
+          <Route path="/favorites" element={<FavoritesPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
