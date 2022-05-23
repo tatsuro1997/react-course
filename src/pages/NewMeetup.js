9b4aeaf2abc069e65a0b95589b5210ef7711c3f0
tatsuro1997 <minagawa@dptr.jp>
2022-05-24 05:47:00 +0900

    Add useNavigate

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,6 +1,10 @@
+import { useNavigate } from "react-router-dom";
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+  const navigate = useNavigate();
+
   function addMeetupHandler(meetupData) {
     fetch(
       "https://react-getting-started-6d863-default-rtdb.firebaseio.com/meetups.json",
@@ -11,7 +15,9 @@ function NewMeetupPage() {
           "Content-Type": "application/json",
         },
       }
-    );
+    ).then(() => {
+      navigate("/");
+    });
   }
 
   return (
