ce70762ab15c10d12baedf10fbdb8bcff1d135bb
tatsuro1997 <minagawa@dptr.jp>
2022-05-24 06:45:11 +0900

    More Context Usage

--- src/pages/FavoritesMeetup.js
+++ src/pages/FavoritesMeetup.js
@@ -1,5 +1,25 @@
+import { useContext } from "react";
+import MeetupList from "../components/meetups/MeetupList";
+
+import FavoritesContext from "../store/faorites-context";
+
 function FavoritesPage() {
-  return <div>FavoritesPage</div>;
+  const favoritesCtx = useContext(FavoritesContext);
+
+  let content;
+
+  if (favoritesCtx.totalFavorites === 0) {
+    content = <p>You got no favotites yet. Start adding some?</p>;
+  } else {
+    content = <MeetupList meetups={favoritesCtx.favorites} />;
+  }
+
+  return (
+    <section>
+          <h1>My Favorites</h1>
+          {content}
+    </section>
+  );
 }
 
 export default FavoritesPage;
