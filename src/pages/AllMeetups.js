3d858e9b902f6a5f9881fd9f5f7e8344a200be73
tatsuro1997 <minagawa@dptr.jp>
2022-05-24 06:03:37 +0900

    Using the useEffect

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,21 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import MeetupList from "../components/meetups/MeetupList";
 
 function AllMeetupsPage() {
-    const [isLoading, setIsLoading] = useState(true);
-    const [loadedMeetups, setLoadedMeetups] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
 
-  fetch(
-    "https://react-getting-started-6d863-default-rtdb.firebaseio.com/meetups.json"
-  )
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      "https://react-getting-started-6d863-default-rtdb.firebaseio.com/meetups.json"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const meetups = [];
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key],
+          };
+
+          meetups.push(meetup);
+        }
         setIsLoading(false);
-        setLoadedMeetups(data);
-    });
+        setLoadedMeetups(meetups);
+      });
+  }, []);
 
   if (isLoading) {
     return (
